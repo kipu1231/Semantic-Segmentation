fa51bea8df28c1e9d4b1f1db18d3bbe2d7956070
Chiara Pullem <chiara.pullem@gmx.de>
2020-03-23 17:59:17 +0100

    updated links to final models

--- test_model_baseline.sh
+++ test_model_baseline.sh
@@ -1,4 +1,4 @@
-# TODO: create shell script for running the testing code of the baseline model
-wget https://www.dropbox.com/s/4tqbbysa6r0lu7s/model_best.pth-12.tar?dl=1
+#shell script for running the testing code of the baseline model
+wget https://www.dropbox.com/s/l00ql1qe4cahqmg/model_best.pth-12.tar?dl=1
 RESUME='model_best.pth-12.tar?dl=1'
 python test.py --resume $RESUME --data_dir $1 --save_dir $2
--- test_model_best.sh
+++ test_model_best.sh
@@ -1,4 +1,4 @@
-# TODO: create shell script for running the testing code of the baseline model
-wget https://www.dropbox.com/s/mhxobhsraafrq3r/model_best.pth-17.tar?dl=1
+#shell script for running the testing code of the baseline model
+wget https://www.dropbox.com/s/01v3jog9rq2nkzx/model_best.pth-17.tar?dl=1
 RESUME='model_best.pth-17.tar?dl=1'
 python test.py --resume $RESUME --data_dir $1 --save_dir $2
